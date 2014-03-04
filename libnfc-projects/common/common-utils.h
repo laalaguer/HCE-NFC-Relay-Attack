0f450cf09e3aa6636d8d16a8b341a29041691180
laalaguer <laalaguer@gmail.com>
2014-03-04 15:58:57 +0200

    add libnfc reader

---
+++ libnfc-projects/common/common-utils.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_hexdump(const unsigned char *pbtData, const size_t szData);
+void string_to_byteArray(char *string, unsigned char *dest, size_t *len);
+
