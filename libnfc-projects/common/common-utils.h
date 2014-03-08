e36458349996b619544beb4269fd8a76858f2bb8
laalaguer <laalaguer@gmail.com>
2014-03-08 21:01:27 +0200

    Remove Private Libnfc-projects

--- libnfc-projects/common/common-utils.h
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void print_hexdump(const unsigned char *pbtData, const size_t szData);
-void string_to_byteArray(char *string, unsigned char *dest, size_t *len);
-
